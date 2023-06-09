06755b7963d9712bb7e33d1aa97ed69add53b228
d.vasylyna <dmytro.vasylyna@mgid.com>
2023-06-09 17:38:24 +0300

    refactor

--- models.ts
+++ models.ts
@@ -1,14 +1,9 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { CreativeStatus } from '@mgx/pub/core/api';
 
-export type MediaLibraryType = typeof MediaLibraryType[keyof typeof MediaLibraryType];
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export const MediaLibraryType = {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   StaticImage: 'static-image',
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   Video: 'video',
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   Animation: 'animation',
 } as const;
 
